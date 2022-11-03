98a48ff73ee889813e2185f4bec87cbb1d24a9c3
ituchkub <ituchkub@hotmail.com>
2022-11-03 16:19:37 +0700

    Update service.js

--- node_api_s/services/service.js
+++ node_api_s/services/service.js
@@ -1,11 +1,41 @@
 require('dotenv').config();
 const fs = require('fs');
 const uuid = require('uuid');
+const resizeImg = require('resize-image-buffer');
+const sharp = require('sharp')
 class Service {
+
+
+  async compressimages(buffer, newSize) {
+
+    try {
+      const image_newSize = sharp(buffer)
+        .jpeg({ quality: 10 })
+      newSize(image_newSize)
+    } catch (err) {
+    }
+  }
+
+  async resizeImgs(buffer, newSize) {
+    try {
+      const image_newSize = await resizeImg(buffer, {
+        width: 128,
+        height: 128,
+      });
+      newSize(image_newSize)
+    } catch (err) {
+
+    }
+  }
+
+
+
   async get(req) {
     return 'This is my API running...'
-    
-  }    
+
+  }
+
+
   async WFile(req) {
     let fileName = uuid.v1();
     fs.writeFile("/Result/" + fileName + ".txt", "Test Write File !!!", function (err) {
@@ -14,12 +44,12 @@ class Service {
       }
       console.log("The file was saved!");
     });
-    
+
   }
   async postEx(req) {
     let Name = req.body.name;
     let mail = req.body.mail;
-    return('This is my Name  : ' + Name + ' And My E-Mail : ' + mail)
+    return ('This is my Name  : ' + Name + ' And My E-Mail : ' + mail)
     //res.send(req.body)    {"name":"Kantathus","mail":"ituchkub@gmail.com"}
   }
   async Author(req) {
